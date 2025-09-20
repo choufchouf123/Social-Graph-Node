41caa71e2f5e6429858b42b4db9fadf65ed0bb20
Rayan Choufani <68738351+choufchouf123@users.noreply.github.com>
2025-09-20 17:04:52 +0200

    Cleaned out code in DataBaseVisualizer

--- Assets/Scripts/VisualGraph/DataBaseVisualizer.cs
+++ Assets/Scripts/VisualGraph/DataBaseVisualizer.cs
@@ -23,23 +23,28 @@ public class DataBaseVisualizer : MonoBehaviour
             GameObject userNode = Instantiate(_userNodeObj, transform);
             userNode.name = user.Name;
             _userNodes.Add(user.Id, userNode);
+
             //temporary random position
             userNode.transform.localPosition = new Vector3(Random.Range(-50, 50), Random.Range(-50, 50), 0);
         }
     }
     private void CreateConnections() {
+        //forach user in the database, get all connections and create lines between them
         foreach (User user in _users) {
             List<UserConnections> lConnectionsToUser = DataBaseManager.instance.GetUserConnections(user);
+
+            //Scale the user node based on the number of connections
             _userNodes[user.Id].transform.localScale *= 1 + (lConnectionsToUser.Count * 0.2f);
+
+            //Create a line between the user and each connection
             foreach (UserConnections connection in lConnectionsToUser) {
 
                 User connectedUser = DataBaseManager.instance.GetUserById(connection.UserIdB);
 
-                if (_userNodes.ContainsKey(user.Id)) {
-                    VisualLineConnecter line = Instantiate(_connectionLineObj, transform);
-                    line.pointA = _userNodes[user.Id].transform;
-                    line.pointB = _userNodes[connectedUser.Id].transform;
-                }
+                if (_userNodes.ContainsKey(user.Id)) return;
+                VisualLineConnecter line = Instantiate(_connectionLineObj, transform);
+                line.pointA = _userNodes[user.Id].transform;
+                line.pointB = _userNodes[connectedUser.Id].transform;
             }
         }
     }
